9fdbbcb4841e76dc7ee397554aafd59f48680d57
Christoph Herrmann <developer@dragonprojects.de>
2015-02-23 21:18:47 +0100

    Formatting and remove the JSHint comments

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 "use strict";
-/*global __dirname:false */
 
 // Load the libraries and modules
 
--- modules/config.js
+++ modules/config.js
@@ -1,5 +1,4 @@
 "use strict";
-/*global module:false */
 
 /**
  * Serves the config for the application
